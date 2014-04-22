6a2d6c8e8105e43fd294c5ea9aa21f9327bc2b49
mirakulo <msonevytsky@yahoo.com>
2014-04-21 21:46:58 -0400

    commit practice

--- js/main.js
+++ js/main.js
@@ -2,4 +2,6 @@ console.log("hello");
 var cvs = document.getElementById("cvs");
 var ctx = cvs.getContext("2d");
 ctx.fillStyle = "#D72700";
-ctx.fillRect(0,0,40,80);
+ctx.fillRect(50,75,10,40);
+ctx.fillStyle = "black";
+ctx.fillRect(47,60,15,15);

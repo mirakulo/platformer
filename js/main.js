6d768a0cb3f20165e3ccdcec339e798566d51193
mirakulo <msonevytsky@yahoo.com>
2014-04-22 21:04:39 -0400

    he is moving

--- js/main.js
+++ js/main.js
@@ -1,7 +1,33 @@
 console.log("hello");
 var cvs = document.getElementById("cvs");
 var ctx = cvs.getContext("2d");
-ctx.fillStyle = "#D72700";
-ctx.fillRect(50,75,10,40);
-ctx.fillStyle = "black";
-ctx.fillRect(47,60,15,15);
+
+var character = {
+	name: "Beamio",
+	xPos: 47,
+	yPos: 60,
+	render: function() {
+		cvs.width = cvs.width;
+		ctx.fillStyle = "#D72700";
+		ctx.fillRect(this.xPos + 3,this.yPos + 15,10,40);
+		ctx.fillStyle = "black";
+		ctx.fillRect(this.xPos,this.yPos,15,15);
+	}
+}
+
+document.addEventListener("keydown", function(event){
+	switch(event.keyCode){
+		// Left arrow
+		case 37:
+			console.log('left');
+			character.xPos -= 5;
+			character.render();
+		break;		
+		// Right arrow
+		case 39:
+			console.log('right');
+			character.xPos += 5;
+			character.render();
+		break;
+	}
+});
